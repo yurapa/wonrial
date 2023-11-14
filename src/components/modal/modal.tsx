ac206c57aba17f76e56a0ff0194e096e07da01fa
yurapa <yura.paray@gmail.com>
2023-11-15 01:24:17 +0200

    updated the portal for modal popup as HOC

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -5,10 +5,12 @@ import styles from './modal.module.scss';
 
 export default function Modal({
   title,
+  description,
   children,
   onClose,
 }: {
   title?: string;
+  description?: string;
   children: ReactNode;
   onClose: () => void;
 }) {
@@ -18,8 +20,8 @@ export default function Modal({
 
       <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
         <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-          <div className="relative transform overflow-hidden rounded-lg bg-white dark:bg-dark text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-            <div className="bg-white dark:bg-dark px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+          <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all dark:bg-dark sm:my-8 sm:w-full sm:max-w-lg">
+            <div className="bg-white px-4 pb-4 pt-5 dark:bg-dark sm:p-6 sm:pb-4">
               <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
                 <div className={styles.modal__header}>
                   <AiOutlineCloseCircle color="black" onClick={onClose} />
@@ -29,6 +31,10 @@ export default function Modal({
                   <h3 className="mb-3 text-center text-2xl font-bold text-black dark:text-white sm:text-3xl">
                     {title}
                   </h3>
+                )}
+
+                {description && (
+                  <p className="mb-11 text-center text-base font-medium text-body-color">{description}</p>
                 )}
 
                 <div className="mt-2">{children}</div>
---
+++ src/utils/portal.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import { ReactNode, useEffect, useState } from 'react';
+import { createPortal } from 'react-dom';
+
+export default function Portal({ children }: { children: ReactNode }) {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+
+    return () => setMounted(false);
+  }, []);
+
+  return mounted ? createPortal(children, document.getElementById('modal-root') as HTMLElement) : null;
+}
