1c6d421f22829e89a5fb435635ac703bc20456ce
yurapa <yura.paray@gmail.com>
2025-09-25 22:52:00 +0300

    Updated domain

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://www.wonrial.com/sitemap.xml',
+    sitemap: 'https://wonrial.com/sitemap.xml',
   };
 }
